d951875380162f2f60d35203370a4a3e79faa15f
Juan Cuevas <juanandrescuevas14@gmail.com>
2024-02-07 20:28:00 -0400

    Actualización de la función de búsqueda A*. Falta la heuristica

---
+++ global/astar.hpp
@@ -0,0 +1,16 @@
+#ifndef ASTAR_HPP
+#define ASTAR_HPP
+
+
+#include "../src/psvn.hpp"
+
+using namespace std;
+
+Node *astar_search(state_t *initial_state, 
+    heuristic_t heuristic,
+    int64_t *num_generated_states,
+    int64_t *num_expanded_states, 
+    int64_t *max_depth
+    int time_limit);
+
+#endif  // ASTAR_HPP
