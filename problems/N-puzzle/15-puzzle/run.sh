f8d819773d3a86b1b37c30480291ea8476abd73d
Ana <73360276+anna-ayn@users.noreply.github.com>
2024-02-09 21:55:19 -0400

    shell

---
+++ problems/N-puzzle/15-puzzle/run.sh
@@ -0,0 +1,6 @@
+make 15-puzzle/test1.pdb
+make 15-puzzle/test2.pdb
+make 15-puzzle/test3.pdb
+
+make 15-puzzle.idastar
+./15-puzzle.idastar < initialstates.txt
---
+++ problems/N-puzzle/24-puzzle/run.sh
@@ -0,0 +1,8 @@
+make 24-puzzle/test1.pdb
+make 24-puzzle/test2.pdb
+make 24-puzzle/test3.pdb
+make 24-puzzle/test4.pdb
+make 24-puzzle/test5.pdb
+
+make 24-puzzle.idastar
+./24-puzzle.idastar < initialstates.txt
