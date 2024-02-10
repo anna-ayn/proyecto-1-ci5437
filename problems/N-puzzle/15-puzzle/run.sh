8a75b940abcacc0fb86c03b3767e1ef91cc94eb5
Simon <18-10717@usb.ve>
2024-02-10 17:51:50 -0400

    ADDED: 15-puzzle results

--- problems/N-puzzle/15-puzzle/run.sh
+++ problems/N-puzzle/15-puzzle/run.sh
@@ -2,5 +2,10 @@ make 15-puzzle/test1.pdb
 make 15-puzzle/test2.pdb
 make 15-puzzle/test3.pdb
 
+echo "idastar 15-puzzle"
 make 15-puzzle.idastar
 ./15-puzzle.idastar < initialstates.txt
+
+echo "A* 15-puzzle (Manhattan Distance)"
+make 15-puzzle.astarM
+./15-puzzle.astar < initialstates.txt
