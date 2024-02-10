edb4598a0ca007a0862e075b0f0a07e06862656d
Juan Cuevas <juanandrescuevas14@gmail.com>
2024-02-10 13:46:19 -0400

    Actualización de algoritmo A* y estructuras de datos

--- global/astar.cpp
+++ global/astar.cpp
@@ -1,17 +1,84 @@
-#include "../src/psvn.cpp"
 #include <limits.h>
 
 #include <iostream>
 #include <queue>
 #include <unordered_map>
+#include <chrono>
+#include "astar.hpp"
 
 #define INFINITY UINT_MAX
 
 using namespace std;
 
-// ! Para que compile
-unsigned int heuristic(state_t *state){
-    return 0;
+Node *make_root_node(state_t *state) {
+  Node *node = new Node;
+  node->state = state;
+  node->parent = NULL;
+  node->action = -1;
+  node->g = 0;
+  node->depth = 0;
+  return node;
+}
+
+Node *make_node(Node *parent, state_t *state, Action action) {
+  Node *node = new Node;
+  node->state = state;
+  node->parent = parent;
+  node->action = action;
+  node->g = parent->g + get_fwd_rule_cost(action);
+  node->depth = parent->depth + 1;
+  return node;
+}
+
+vector<pair<state_t *, Action>> *successors(state_t *state) {
+  vector<pair<state_t *, Action>> *successors_list =
+      new vector<pair<state_t *, Action>>;
+  if (successors_list == NULL) {
+    fprintf(stderr, "out of memory\n");
+    return NULL;
+  }
+  ruleid_iterator_t *iter = new ruleid_iterator_t;
+  if (iter == NULL) {
+    fprintf(stderr, "out of memory\n");
+    return NULL;
+  }
+  int move_cost, ruleid;
+  int hist, child_hist;
+  hist = init_history;
+  init_fwd_iter(iter, state);
+  while ((ruleid = next_ruleid(iter)) >= 0) {
+    if (!fwd_rule_valid_for_history(hist, ruleid)) {
+      continue;
+    }
+    state_t *child = (state_t *)(malloc(sizeof(state_t)));
+    if (child == NULL) {
+      fprintf(stderr, "out of memory\n");
+      return NULL;
+    }
+    child_hist = next_fwd_history(hist, ruleid);
+    apply_fwd_rule(ruleid, state, child);
+    // oprint_state(child);
+    //     move_cost = get_fwd_rule_cost(ruleid);
+    successors_list->push_back(make_pair(child, ruleid));
+  }
+  return successors_list;
+}
+
+void set_distance(state_t *state, unsigned distance, unordered_map<string, unsigned> &cost_so_far) {
+  char *str_state = new char[1024];
+  sprint_state(str_state, 1024, state);
+  cost_so_far[string(str_state)] = distance;
+}
+
+unsigned get_distance(state_t *state, unordered_map<string, unsigned> &cost_so_far) {
+  char *str_state = new char[1024];
+  sprint_state(str_state, 1024, state);
+  // detect if the state is not in the map
+  if (cost_so_far.find(string(str_state)) == cost_so_far.end()) {
+    return UINT_MAX;
+  }
+  unsigned r = cost_so_far[string(str_state)];
+  return r;
 }
 
 /*
@@ -25,18 +92,15 @@ Recibe:
 */
 Node *astar_search(state_t *initial_state, int64_t *num_generated_states, int64_t *num_expanded_states,  int64_t *max_depth, int time_limit){
     // Inicializamos el tiempo
-    time_t start, finish;
-    time(&start);
-    time(&finish);
+    auto start = chrono::high_resolution_clock::now(), finish = chrono::high_resolution_clock::now();
     // Inicializamos el mapa de costos y la cola de prioridad
     unordered_map<string, unsigned> cost_so_far;  // visited,
     priority_queue<pair<int, Node *>, vector<pair<int, Node *>>, greater<pair<int, Node *>>> q;
     // Inicializamos el nodo raiz y lo agregamos a la cola de prioridad
     Node *root = make_root_node(initial_state);
     q.push(make_pair(heuristic(initial_state), root));
-    // Mientras la cola de prioridad no este vacia
-    while (!q.empty() && difftime(finish, start) < time_limit){
-        time(&finish);
+    // Mientras la cola de prioridad no este vacia y no se haya terminado el tiempo
+    while (!q.empty() && chrono::duration_cast<chrono::minutes>(finish - start).count() < time_limit){
         // Sacamos el nodo con menor costo de la cola de prioridad
         Node *n = q.top().second;
         q.pop();
@@ -71,6 +135,8 @@ Node *astar_search(state_t *initial_state, int64_t *num_generated_states, int64_
                 }
             }
         }
+        // Actualizamos el tiempo
+        finish = chrono::high_resolution_clock::now();
     }
   return nullptr;  // no solution found
 }
