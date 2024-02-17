e7add5ee6668bd7a8fbf9d572b29b07991e3fae4
Juan Cuevas <juanandrescuevas14@gmail.com>
2024-02-16 20:25:00 -0400

    Agregados archivos de resultados de ejecución y corregidos errores en el script de ejecución

--- problems/N-puzzle/24-puzzle/run.sh
+++ problems/N-puzzle/24-puzzle/run.sh
@@ -7,8 +7,8 @@ make 24-puzzle/test6.pdb
 
 echo "A* 24-puzzle (PDBs aditivos)"
 make 24-puzzle.astar
-./24-puzzle.astar < initialstates.txt
+./24-puzzle.astar < initialstates2.txt
 
 echo "IDA* 24-puzzle (PDBs aditivos)"
 make 24-puzzle.idastar
-./24-puzzle.idastar < initialstates.txt
+./24-puzzle.idastar < initialstates2.txt
